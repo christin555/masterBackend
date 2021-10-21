126a805baccbe9b96d58c977f784ce45da59b16f
unknown <neosat55@gmail.com>
2021-10-21 22:05:40 +0500

    Return full save

--- service/parsing/insertToBd.js
+++ service/parsing/insertToBd.js
@@ -10,36 +10,36 @@ module.exports = {
             .merge()
             .returning(['id', 'name', 'code']);
 
-        // const imgsObject = array2Object(imgs, 'name');
-        //
-        // const imgToInsert = products.reduce((array, {name, id}) => {
-        //     const imgsProducts = imgsObject[name].imgs.map((img, index) => {
-        //         return {
-        //             entity: entity.PRODUCT,
-        //             entityId: id,
-        //             src: `${process.env.APP_HOST_URL}/public/imgs/original/${entity.PRODUCT}_${id}_${index}.jpg`,
-        //             isDoor: imgsObject[name].isDoor,
-        //             path: `public/imgs/original/${entity.PRODUCT}_${id}_${index}.jpg`,
-        //             origSrc: img
-        //         };
-        //     });
-        //
-        //     imgsProducts[0].isMain = true;
-        //     imgsProducts[1] && (imgsProducts[1].isForHover = true);
-        //
-        //     array.push(...imgsProducts);
-        //
-        //     return array;
-        // }, []);
-        //
-        // Promise.all(imgToInsert.map(({path, origSrc}) => saveImg(origSrc, path)));
+        const imgsObject = array2Object(imgs, 'name');
 
-        //когда-нибудь переделай, пожалуйста, ну реально кровь из глаз идет аж...
-        // await knex('media')
-        //     .insert(
-        //         imgToInsert.map(({entity, entityId, src, isDoor, isMain, isForHover}) => {
-        //             return {entity, entityId, src, isDoor, isMain, isForHover};
-        //         }));
+        const imgToInsert = products.reduce((array, {name, id}) => {
+            const imgsProducts = imgsObject[name].imgs.map((img, index) => {
+                return {
+                    entity: entity.PRODUCT,
+                    entityId: id,
+                    src: `${process.env.APP_HOST_URL}/public/imgs/original/${entity.PRODUCT}_${id}_${index}.jpg`,
+                    isDoor: imgsObject[name].isDoor,
+                    path: `public/imgs/original/${entity.PRODUCT}_${id}_${index}.jpg`,
+                    origSrc: img
+                };
+            });
+
+            imgsProducts[0].isMain = true;
+            imgsProducts[1] && (imgsProducts[1].isForHover = true);
+
+            array.push(...imgsProducts);
+
+            return array;
+        }, []);
+
+        Promise.all(imgToInsert.map(({path, origSrc}) => saveImg(origSrc, path)));
+
+        // когда-нибудь переделай, пожалуйста, ну реально кровь из глаз идет аж...
+        await knex('media')
+            .insert(
+                imgToInsert.map(({entity, entityId, src, isDoor, isMain, isForHover}) => {
+                    return {entity, entityId, src, isDoor, isMain, isForHover};
+                }));
 
         if (prices) {
             const pricesProducts = products.map(({id, code}) => {
