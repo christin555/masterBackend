514109e562ec7ecc95f66cf45ace8854b9e94d3a
Крис <kriska_555@mail.ru>
2022-04-13 14:39:01 +0500

    linit

---
+++ migrations/20220413110158_addSalePrice.js
@@ -0,0 +1,13 @@
+
+exports.up = (knex) => knex.schema.alterTable('prices', (table) => {
+    table.string('salePrice')
+        .comment('Цена со скидкой');
+    table.string('salePercent')
+        .comment('Процент скидки');
+});
+
+
+exports.down = (knex) => knex.schema.alterTable('prices', (table) => {
+    table.dropColumns('salePrice');
+ //   table.dropColumns('salePercent');
+});
