3dcd1b9d7f791e1e5a2cb1f769ec4f14dfaa7fe5
Крис <kriska_555@mail.ru>
2022-06-14 21:04:12 +0500

    *

--- migrations/20220614142949_trgExt.js
+++ migrations/20220614142949_trgExt.js
@@ -1,6 +1,6 @@
 exports.up = (knex) => knex.raw(`
     CREATE if not exists extension pg_trgm;
-    CREATE if not exists INDEX trgm_idx_gin ON public.products USING gin ("searchKeys" gin_trgm_ops);
+    CREATE INDEX if not exists trgm_idx_gin ON public.products USING gin ("searchKeys" gin_trgm_ops);
     SET pg_trgm.strict_word_similarity_threshold = 0.3;
     `);
 
